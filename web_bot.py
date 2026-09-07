6279f5f1c78cfc6dfd088dd0d42164cb8e45faa8
alkarbi-spec <alkarbi@mail.ru>
2026-09-07 15:25:29 +0300

    Update web_bot.py

--- web_bot.py
+++ web_bot.py
@@ -7,7 +7,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 
-TOKEN = "ТВОЙ_НОВЫЙ_ТОКЕН"
+TOKEN = "8829675416:AAEQqRRlK3CGxCv0I253urtJreDV7pT_gOM"
 GROUP_ID = -1003218790551
 PUBLIC_CHAT_ID = -1003503911588
 
